7488a8367d4877bc2bd51dbd579cca370abf95aa
DheerajPatil-Coder <dheerajpatil015@gmail.com>
2021-10-06 17:29:40 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Button = ({ color, text, onClick }) => {
+  return (
+    <>
+      <button
+        style={{ backgroundColor: color }}
+        className="btn"
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onclick: PropTypes.func,
+};
+
+export default Button;
